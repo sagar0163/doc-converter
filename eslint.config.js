537722062174301b9be451e350c527c59767c6bd
Sagar Jadhav <sagarjadhav063@gmail.com>
2026-02-21 23:30:52 +0530

    ci: add ESLint configuration
    - Use recommended ESLint config
    - Allow console for CLI
    - Warn on unused vars

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2024,
+      sourceType: 'module',
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+];
